8dea591ccf4391e97cbd8d0a0402d7195f5db5ed
binary-maksym <maksym@binary.com>
2016-02-05 13:40:33 +0800

    new functional

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import configureStore from './lib/store/configureStore.js';
-import {getSymbols,setSymbol} from './lib/actions/symbols' 
+import {
+    getSymbols, setSymbol, requestSymbols
+}
+from './lib/actions/symbols'
 
 let store = configureStore();
-store.dispatch(setSymbol('R_50'))
+
+store.dispatch(requestSymbols())
+
+// store.dispatch(setSymbol('lol2'))
+// store.dispatch(setSymbol('R_25'))
+// store.dispatch(setSymbol('lol3'))
