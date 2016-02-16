ee831221460d63dcc8df592b996b8070f14d83f5
binary-maksym <maksym@binary.com>
2016-02-16 10:41:50 +0800

    updates

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 import configureStore from './lib/store/configureStore.js';
-import action_creators from './lib/actions/core'
+import action_creators from './lib/actions/root'
 
 let store = configureStore();
-// store.dispatch(action_creators.getSymbols())
-
-// store.dispatch(setSymbol('lol2'))
 
 store.dispatch(action_creators.setMarket("random"))
 store.dispatch(action_creators.setSymbol('R_25'))
-// store.dispatch(action_creators.setMarket("random"))
-// store.dispatch(action_creators.setSymbol("R_500"))
-// store.dispatch(action_creators.getContracts("R_50"))
 store.dispatch(action_creators.getContracts())
