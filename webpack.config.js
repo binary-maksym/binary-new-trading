16e1247c2d96e1909ceafd488c5aa389d098a75b
binary-maksym <maksym@binary.com>
2016-03-19 10:54:20 +0800

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,41 @@
 var webpack = require('webpack');
 
 module.exports = {
-    entry: [
-        'webpack-hot-middleware/client',
-        'babel-polyfill',
-        './lib/index'
-    ],
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            // loader: 'react-hot!babel',
-            loader: 'babel-loader',
-            query: {
-                plugins: ['transform-runtime'],
-                presets: ['es2015', 'stage-0', 'react'],
-            }
-        }, {
-            test: /\.json$/,
-            exclude: /node_modules/,
-            loader: 'json-loader',
-        }]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx', '.json']
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ]
+  entry: [
+    'webpack-hot-middleware/client',
+    'babel-polyfill',
+    './lib/index',
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      // loader: 'react-hot!babel',
+      loaders: ["babel-loader?" + JSON.stringify({
+        plugins: ['transform-runtime'],
+        presets: ['es2015', 'stage-0', 'react'],
+      }), "eslint-loader"],
+    }, {
+      test: /\.json$/,
+      exclude: /node_modules/,
+      loader: 'json-loader',
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json'],
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ]
 };
